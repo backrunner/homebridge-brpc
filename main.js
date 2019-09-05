19152fcd754c244c5205dd1b486497b2bb490bc0
BackRunner <liaoyp126@126.com>
2019-09-05 09:27:38 +0800

    init

---
+++ main.js
@@ -0,0 +1,119 @@
+const wol = require('node-wol');
+const request = require('request');
+
+var Service;
+var Characteristic;
+
+var timer;
+
+const Status = {
+    Online: Symbol('Online'),
+    Offline: Symbol('Offline'),
+    WakingUp: Symbol('Waking Up'),
+    ShuttingDown: Symbol('Shutting Down'),
+    Unknown: Symbol('Unknown')
+};
+
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory('homebridge-brpc', 'PC', HomeBridgePC);
+};
+
+function HomeBridgePC(log, config) {
+    this.log = log;
+    this.name = config.name;
+    this.infomationService = new Service.AccessoryInformation();
+    this.infomation = {
+        serialNumber: "Unknown",
+        name: (config.name ? config.name : "PC"),
+        model: (config.model ? config.model : "PC"),
+        manufacturer: (config.manufacturer ? config.manufacturer : "Unknown"),
+        softwareVersion: (config.software ? config.software : "Unknown")
+    };
+    // set infomation
+    infomationService.setCharacteristic(Characteristic.Manufacturer, this.infomation.manufacturer)
+        .setCharacteristic(Characteristic.Model, this, infomation.model)
+        .setCharacteristic(Characteristic.SerialNumber, this.infomation.serialNumber)
+        .setCharacteristic(Characteristic.Name, this.infomation.name)
+        .setCharacteristic(Characteristic.SoftwareRevision, this.infomation.softwareVersion);
+    // set switchService
+    this.switchService = new Service.Switch(this.name);
+    this.switchService.getCharacteristic(Characteristic.On)
+        .on('get', this.getStatus.bind(this))
+        .on('set', this.setStatus.bind(this));
+    // PC配置
+    this.pc_mac = config.pc_mac;
+    this.service_url = config.service_url;
+    this.auth_username = config.auth_username;
+    this.auth_key = config.auth_key;
+    // 基本信息
+    this.status = Status.Offline;
+}
+
+HomeBridgePC.prototype.getStatus = function(callback){
+    callback(null, this.status === Status.Online || this.status === Status.WakingUp); 
+};
+
+HomeBridgePC.prototype.setStatus = function(on, callback) {
+    if (on){
+        // 打开设备
+        if (this.pc_mac){
+            // 通过wol唤醒
+            this.status = Status.WakingUp;
+            wol.wake(this.pc_mac, function(err){
+                if (err){
+                    // 唤醒失败，设置状态为关闭
+                    this.status = Status.Offline;
+                } else {
+                    // 设置 Timer
+                    if (timer){
+                        clearInterval(timer);
+                    }
+                    timer = setInterval(function(){
+                        request.get(this.service_url + '/ping', {
+                            timeout: 5000
+                        }, function(error, response){
+                            if (error){
+                                this.status = Status.Offline;
+                                return;
+                            }
+                            if (response.statusCode == 200){
+                                this.status = Status.Online;
+                            } else {
+                                this.status = Status.Unknown;
+                            }
+                        }.bind(this));
+                    }.bind(this), 5000);
+                }
+            }.bind(this));
+        } else {
+            callback(new Error("PC MAC cannot be empty."));
+            return;
+        }
+    } else {
+        // 关闭设备
+        if (this.service_url){
+            if (timer){
+                clearInterval(timer);
+            }
+            request.get({
+                url: this.service_url + '/action/shutdown',
+                headers: {
+                    'Authorization': this.auth_username+':'+this.auth_key
+                }
+            }, function(error, response, body){
+                if (response.statusCode == 200){
+                    this.status = Status.ShuttingDown;
+                }
+            }.bind(this));
+        } else {
+            callback(new Error("Service URL cannot be empty."));
+            return;
+        }
+    }
+};
+
+HomeBridgePC.prototype.getServices = function() {
+    return [this.infomationService, this.switchService];
+};
